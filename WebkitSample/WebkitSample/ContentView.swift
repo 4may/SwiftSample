84aea826b847c7df11b10c90830677f8c4c29317
Naoki Hanzawa <naoki.hanzawa.pub@gmail.com>
2020-01-30 15:24:37 +0900

    sample projectを作成

---
+++ WebkitSample/WebkitSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebkitSample
+//
+//  Created by Naoki Hanzawa on 2020/01/30.
+//  Copyright © 2020 Naoki Hanzawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
